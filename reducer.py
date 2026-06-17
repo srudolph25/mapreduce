642e48bd6319350eae4d9afd263c7371f86e02ae
Sabina Rudolph <s_rudolphs25@stud.hwr-berlin.de>
2026-06-17 16:33:16 +0200

    Calculate average sales by category

--- reducer.py
+++ reducer.py
@@ -3,19 +3,25 @@
 import sys
 
 current_key = None
+current_sales = 0.0
 current_count = 0
 
 for line in sys.stdin:
     line = line.strip()
     key, value = line.split("\t", 1)
+    sales = float(value)
 
     if current_key == key:
+        current_sales += sales
         current_count += 1
     else:
-        if current_key and current_count > 114:
-            sys.stdout.write("{0}\t{1}\n".format(current_key, current_count))
+        if current_key:
+            average = current_sales / current_count
+            sys.stdout.write("{0}\t{1:.2f}\n".format(current_key, average))
         current_key = key
+        current_sales = sales
         current_count = 1
 
-if current_key and current_count > 114:
-    sys.stdout.write("{0}\t{1}\n".format(current_key, current_count))
+if current_key:
+    average = current_sales / current_count
+    sys.stdout.write("{0}\t{1:.2f}\n".format(current_key, average))
